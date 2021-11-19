b48d8c93458f863308314f42e5a02b84a7145b52
Aaron Kirby <APKirby@nationalindemnity.com>
2021-11-19 11:26:40 -0600

    Finished date

--- MyWork/app.js
+++ MyWork/app.js
@@ -21,3 +21,19 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2020, 4, 24, 11, 30, 0); // months are 0 index based, hours are military
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const date = futureDate.getDate();
+const month = months[futureDate.getMonth()];
+const weekday = weekdays[futureDate.getDay()];
+
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${month} ${date} ${year}, ${hours}:${minutes}am`;
